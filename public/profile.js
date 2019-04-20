87e23feb4054b4c25bbe327a6f40597875ab1a7a
Sherman77 <sherman77.xu@gmail.com>
2019-04-20 01:23:38 -0400

    New posting function
    Fixed funtion for making new posts to firestore. Now it can add new posts to  firestore for three tags as following:
    1. marketplace_posts
    2. housing_posts
    3. social_posts

--- public/profile.js
+++ public/profile.js
@@ -17,34 +17,129 @@
 
 
 const housing_form  = document.querySelector('#add_housing');
-var selectedFile;
+const market_form = document.querySelector('#add_marketplace');
+const social_form = document.querySelector('#add_social')
+var time = new Date();
+var date = time.getTime();
+
+//saving data for marketplace
+market_form.addEventListener('submit', (e) => {
+    //Prevent the default action
+    e.preventDefault();
+    
+    //Create root reference
+    const ref = firebase.storage().ref();
+    
+    //Select the file
+    const file = document.querySelector('#marketplace_picture').files[0];
+    
+    //Set file name
+    const name = file.name;
+    
+    //Create the task
+    const task = ref.child("/marketplace_posts/" + name).put(file);
+    
+    //Put the pic to firebase 
+    task
+        .then(snapshort => snapshort.ref.getDownloadURL())
+        .then((url) => {
+            console.log(url);
+            //Create record on firestore
+            db.collection('marketplace_posts').add({
+                adTitle: market_form.title.value,
+                adDescription: market_form.description.value,
+                adPrice: market_form.price.value,
+                imageOne: url,
+                adCategory: market_form.category.value,
+                adStatus: "active",
+                adDate: date,
+        
+            });
+        })
+        .catch(console.error);
+    
+
+})
+
 
 // saving data for housing
 housing_form.addEventListener('submit', (e) => {
+    //Prevent the default action
     e.preventDefault();
+    
+    //Create root reference
+    const ref = firebase.storage().ref();
+    
+    //Select the file
+    const file = document.querySelector('#housing_picture').files[0];
+    
+    //Set file name
+    const name = file.name;
+    
+    //Create the task
+    const task = ref.child("/housing_posts/" + name).put(file);
+    
+    //Put the pic to firebase 
+    task
+        .then(snapshort => snapshort.ref.getDownloadURL())
+        .then((url) => {
+            console.log(url);
+            //Create record on firestore
+            db.collection('housing_posts').add({
+                adTitle: housing_form.title.value,
+                adDescription: housing_form.description.value,
+                adRent: housing_form.rent.value,
+                imageOne: url,
+                adCategory: housing_form.category.value,
+                adStatus: "active",
+                adDate: date,
+        
+            });
+        })
+        .catch(console.error);
+    
 
-    db.collection('Housing').add({
-        title: housing_form.title.value,
-        description: housing_form.description.value,
-        price: housing_form.price.value,
-        picture: housing_form.picture.value,
-        created: firebase.firestore.Timestamp.fromDate(new Date()) 
-
-    });
 })
 
-const social_form  = document.querySelector('#add_social');
-var selectedFile;
+
 
 // saving data for social
 social_form.addEventListener('submit', (e) => {
+    //Prevent the default action
     e.preventDefault();
+    
+    //Create root reference
+    const ref = firebase.storage().ref();
+    
+    //Select the file
+    const file = document.querySelector('#social_picture').files[0];
+    
+    //Set file name
+    const name = file.name;
+    
+    //Create the task
+    const task = ref.child("/social_posts/" + name).put(file);
+    
+    //Put the pic to firebase 
+    task
+        .then(snapshort => snapshort.ref.getDownloadURL())
+        .then((url) => {
+            console.log(url);
 
-    db.collection('Social').add({
-        title: social_form.title.value,
-        description: social_form.description.value,
-        picture: social_form.picture.value,
-        created: firebase.firestore.Timestamp.fromDate(new Date()) 
 
-    });
+            //Create record on firestore
+            db.collection('social_posts').add({
+                adTitle: social_form.title.value,
+                adDescription: social_form.description.value,
+                imageOne: url,
+                adCategory: social_form.category.value,
+                adStatus: "active",
+                adDate: date,
+                eventDate: social_form.datetime.valueAsNumber,
+        
+            });
+        })
+        .catch(console.error);
+    
+
 })
