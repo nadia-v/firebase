70d5d3fe38367dcff0875b2b556769cdf92c09c3
Nadia Vedeneyeva <nadiaveden@gmail.com>
2019-04-21 03:08:14 -0400

    two posts

--- public/marketplace.js
+++ public/marketplace.js
@@ -2,7 +2,7 @@ const marketplace_posts = document.querySelector("#marketplace_posts");
 
 
 db.collection("marketplace_posts").onSnapshot(function(querySnapshot) {
-  
+    
     querySnapshot.forEach(function(doc) {     
 
         const storageRef = firebase.storage().refFromURL(doc.data().imageOne);
@@ -13,11 +13,30 @@ db.collection("marketplace_posts").onSnapshot(function(querySnapshot) {
             "</h2><h2>Description: " + doc.data().adDescription + 
             "</h2><h2>Status: " + doc.data().adStatus + 
             "</h2><h2>Price: " + doc.data().adPrice + 
-            "</h2><h2>Price: " + doc.data().imageOne +
             "</h2><h2>Posted on: " + doc.data().adDate+ "</h2></div>"  
           })
     })
 })
+
+db.collection("marketplace_posts")
+    .orderBy("editDate", "desc")
+    .limit(1).get().then(function(prevSnapshot){
+    
+    prevSnapshot.forEach(function(doc1) {     
+
+        const storageRef = firebase.storage().refFromURL(doc1.data().imageOne);
+        storageRef.getDownloadURL().then(function(url) {       
+            document.getElementById("marketplace-post-image1").src = url;  
+            marketplace_posts1.innerHTML = " <div class=marketplace-posts1><h1>Title: " + doc1.data().adTitle +
+            "</h1><h2>Category: " + doc1.data().adCategory + 
+            "</h2><h2>Description: " + doc1.data().adDescription + 
+            "</h2><h2>Status: " + doc1.data().adStatus + 
+            "</h2><h2>Price: " + doc1.data().adPrice + 
+            "</h2><h2>Posted on: " + doc1.data().adDate+ "</h2></div>"  
+          })
+    })
+})
+
 
 //------------------------------------------------------------------------//
 
